f897e665ee0c6b52a45d7d4cc5436f85237c2bf1
Hinaser <rindo.hinase@gmail.com>
2019-11-29 11:11:49 +0900

    Support idiom

--- src/lib/eowp/index.ts
+++ src/lib/eowp/index.ts
@@ -35,13 +35,22 @@ export type TQATrend = {
   }>;
 };
 
+export function getWordWithKanaTrimmed(el: HTMLElement){
+  const cloneEl = el.cloneNode(true) as HTMLElement;
+  const kanaEls = Array.from(cloneEl.querySelectorAll("span.kana"));
+  kanaEls.forEach(kanaEl => {
+    cloneEl.removeChild(kanaEl);
+  });
+  return cloneEl.innerText;
+}
+
 export function generateWordList(){
   const allRows = document.querySelectorAll("#wordlist > tbody > tr:not(:first-child)");
   
   const WordList: TWord[] = [];
   
-  const wordCount = allRows.length / 2;
-  for(let i=0;i<wordCount;i+=2){
+  const rowCount = allRows.length;
+  for(let i=0;i<rowCount;i+=2){
     const wordRow = allRows[i];
     const meansRow = allRows[i+1];
     
@@ -51,25 +60,40 @@ export function generateWordList(){
     const wordClassEl: HTMLElement[] = wordClassesContainer ?
       Array.from(wordClassesContainer.querySelectorAll(".wordclass")) : [];
     
-    const wordMeans = wordClassEl.map(el => {
-      const wordClass = el.innerText.replace(/[【】]/g, "");
-      const meansArray = el.nextSibling ? Array.from((el.nextSibling as HTMLElement).querySelectorAll("li")) : [];
-      let means;
-      if(meansArray.length > 0){
-        means = meansArray.map(meanEl => {
-          return meanEl.innerText;
-        });
+    const wordMeans = (()=>{
+      if(wordClassEl.length > 0){
+        return wordClassEl.map(el => {
+          const wordClass = el.innerText.replace(/[【】]/g, "");
+          const meansArray = el.nextSibling ? Array.from((el.nextSibling as HTMLElement).querySelectorAll("li")) : [];
+          let means;
+          if(meansArray.length > 0){
+            means = meansArray.map(meanEl => {
+              return getWordWithKanaTrimmed(meanEl);
+            });
+          }
+          else{
+            means = el.nextSibling ? [getWordWithKanaTrimmed(el.nextSibling as HTMLElement)] : [];
+          }
+          return means.map(m => {
+            return {
+              wordClass,
+              mean: m,
+            };
+          });
+        }).flat();
       }
-      else{
-        means = el.nextSibling ? [(el.nextSibling as HTMLElement).innerText] : [];
+      
+      // When not single word
+      if(/[ ]/.test(word) && wordClassesContainer && wordClassesContainer.querySelectorAll("ol").length < 1){
+        return [{
+          wordClass: "熟語",
+          mean: getWordWithKanaTrimmed(wordClassesContainer as HTMLElement),
+        }];
       }
-      return means.map(m => {
-        return {
-          wordClass,
-          mean: m,
-        };
-      });
-    }).flat();
+      
+      console.warn("Unrecognizable word", word);
+      return [];
+    })();
     
     const pronounceEl = meansRow.querySelector<HTMLElement>("span.attr > span.pron");
     const pronounce = pronounceEl ? pronounceEl.innerText.replace(/、$/, "") : "";
